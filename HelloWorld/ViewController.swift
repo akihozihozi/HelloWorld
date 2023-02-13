68b5966e0b2df59e8dac4b20c78469a856811ea0
akitaka.kitahara <akitaka.kitahara@lifestage.one>
2023-02-14 01:22:46 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 北原昂貴 on 2023/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
